451d244ef153b2fef0f240769c2d796de5fdb9b2
Peter Breen <pbreen@gmail.com>
2017-05-30 11:09:25 -0700

    step 2: shape exists, changed color from example :)

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+//
+// Camera and scene
+//
+
 var scene = new THREE.Scene();
 
 //consider modifying this later for a full screen effect
@@ -11,9 +15,40 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 //puts canvas element in DOM
 document.body.appendChild( renderer.domElement );
 
-//see three.js docs on why it uses RAF instead of a SetInterval
+//see https://threejs.org/docs/index.html#manual/introduction/Creating-a-scene section "Rendering a scene" on using RAF vs setInterval
 function render() {
     requestAnimationFrame( render );
     renderer.render( scene, camera );
 }
 render();
+
+
+//
+// Create sugar particle
+//
+
+var mesh;
+var length = 12, width = 8;
+
+//create base shape
+var shape = new THREE.Shape();
+shape.moveTo( 0,0 );
+shape.lineTo( 0, width );
+shape.lineTo( length, width );
+shape.lineTo( length, 0 );
+shape.lineTo( 0, 0 );
+
+//extrude shape (see https://threejs.org/docs/index.html#api/geometries/ExtrudeGeometry)
+var extrudeSettings = {
+  steps: 2,
+  amount: 16,
+  bevelEnabled: true,
+  bevelThickness: 1,
+  bevelSize: 1,
+  bevelSegments: 1
+};
+var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+var material = new THREE.MeshBasicMaterial( { color: 0x007fff } );
+mesh = new THREE.Mesh(geometry, material) ;
+
+scene.add(mesh);
