7ac497f707c0f6d8d652e123fd82cafc0b6560c9
Peter Breen <pbreen@gmail.com>
2017-05-30 11:36:19 -0700

    3 crystals rendered, only one rotates

--- js/app.js
+++ js/app.js
@@ -1,7 +1,8 @@
+var crystals = [];
+
 //////////////////////////////
 // Camera and scene
 //////////////////////////////
-
 var scene = new THREE.Scene();
 
 //consider modifying this later for a full screen effect
@@ -20,45 +21,43 @@ document.body.appendChild( renderer.domElement );
 // All things sugar particle
 //////////////////////////////
 
-var mesh;
-var length = randBetween(1, 6);
-var width = randBetween(1, 8);
+function createMesh() {
+  var length = randBetween(1, 6);
+  var width = randBetween(1, 8);
 
-//create base shape
-var shape = new THREE.Shape();
-shape.moveTo( randBetween(0,1.5),randBetween(0,1.5) );
-shape.lineTo( randBetween(0,1.5), width );
-shape.lineTo( length, width );
-shape.lineTo( length, randBetween(0,1.5) );
-shape.lineTo( randBetween(0,1.5), randBetween(0,1.5) );
+  var shape = new THREE.Shape();
+  shape.moveTo( randBetween(0,1.5),randBetween(0,1.5) );
+  shape.lineTo( randBetween(0,1.5), width );
+  shape.lineTo( length, width );
+  shape.lineTo( length, randBetween(0,1.5) );
+  shape.lineTo( randBetween(0,1.5), randBetween(0,1.5) );
+  var extrudeSettings = {
+    steps: 2,
+    amount: randBetween(3,6),
+    bevelEnabled: true,
+    bevelThickness: 1,
+    bevelSize: randBetween(1,3),
+    bevelSegments: 1.5
+  };
 
-//extrude shape (see https://threejs.org/docs/index.html#api/geometries/ExtrudeGeometry)
-var extrudeSettings = {
-  steps: 2,
-  amount: randBetween(3,6),
-  bevelEnabled: true,
-  bevelThickness: 1,
-  bevelSize: randBetween(1,3),
-  bevelSegments: 1.5
-};
-var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
-var textureLoader = new THREE.TextureLoader();
-textureLoader.crossOrigin = true;
-textureLoader.load('../vendor/sugar-texture-4.png', function(texture) {
-  texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
-  texture.repeat.set(0.1,0.1);
-  var material = new THREE.MeshBasicMaterial({
-    map: texture,
-    transparent: true,
-    premultipliedAlpha: true,
-    side: THREE.doubleSide,
-    blending: THREE.AdditiveBlending
+  var geometry = new THREE.ExtrudeGeometry( shape, extrudeSettings );
+  var textureLoader = new THREE.TextureLoader();
+  textureLoader.crossOrigin = true;
+  textureLoader.load('../vendor/sugar-texture-4.png', function(texture) {
+    texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping;
+    texture.repeat.set(0.1,0.1);
+    var material = new THREE.MeshBasicMaterial({
+      map: texture,
+      transparent: true,
+      premultipliedAlpha: true,
+      side: THREE.DoubleSide,
+      blending: THREE.AdditiveBlending
+    });
+    mesh = new THREE.Mesh(geometry, material) ;
+    mesh.geometry.center();
+    scene.add(mesh);
   });
-  mesh = new THREE.Mesh(geometry, material) ;
-  mesh.geometry.center();
-  scene.add(mesh);
-  render();
-});
+}
 
 //////////////////////////////
 // functions to call elsewhere
@@ -76,3 +75,9 @@ function render() {
 function randBetween(min, max) {
   return (Math.random() * (max - min)) + min;
 }
+
+// call render to get it all working
+for ( var i = 0; i < 3; i ++ ) {
+  createMesh();
+}
+render();
