25cc3556cd9ef93257c66143fd1240ad7865393d
Peter Breen <pbreen@gmail.com>
2017-05-30 10:43:51 -0700

    step one, changed three.js src file

--- js/app.js
+++ js/app.js
@@ -1,1 +1,20 @@
-//project JS goes here
+var THREE = require('three');
+var scene = new THREE.Scene();
+
+//consider modifying this later for a full screen effect
+var camera = new THREE.OrthographicCamera(window.innerWidth / - 2, window.innerWidth / 2, window.innerHeight / 2, window.innerHeight / - 2, - 500, 1000 );
+
+camera.zoom = 5;
+camera.updateProjectionMatrix();
+var renderer = new THREE.WebGLRenderer({alpha: true});
+renderer.setSize( window.innerWidth, window.innerHeight );
+
+//puts canvas element in DOM
+document.body.appendChild( renderer.domElement );
+
+//see three.js docs on why it uses RAF instead of a SetInterval
+function render() {
+    requestAnimationFrame( render );
+    renderer.render( scene, camera );
+}
+render();
